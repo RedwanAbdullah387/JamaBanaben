1537f9bc23dde0143f92090bf4c608c797e50e26
SiyamMia204 <siyam1234mia56789@gmail.com>
2024-11-26 02:52:13 +0600

    test1

---
+++ Test1.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+driver.get("http://127.0.0.1:8000/")
+
+#driver.find_element(By.ID,"")
+time.sleep(2)
+driver.close()
+driver.quit()
+
+print("Done")
